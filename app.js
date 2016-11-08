e4fbcb0de7da4d7964ea18ac514d3649e311fe57
Zachary Jensen <zsjensen@gmail.com>
2016-11-08 16:00:44 -0700

    uploading

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 var express = require('express');
 var app = express();
+var fileUpload = require('express-fileupload');
 var PORT = 80;
+
+app.use(fileUpload());
+ 
+app.post('/upload', function(req, res) {
+	var sampleFile;
+ 
+	if (!req.files) {
+		res.send('No files were uploaded.');
+		return;
+	}
+ 
+	sampleFile = req.files.sampleFile;
+	sampleFile.mv(__dirname + '/upload.fasta.txt', function(err) {
+		if (err) {
+			res.status(500).send(err);
+		}
+		else {
+			res.send('File uploaded!');
+		}
+	});
+});
 
 app.get('/', function (req, res) {
 	res.sendFile( __dirname + '/index.html');
